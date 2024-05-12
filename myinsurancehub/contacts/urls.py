b1ad02f5f7e8d1820f889841ddd267588ba69a93
lghenta <ghenta85@gmail.com>
2024-05-12 22:22:03 +0100

    Add files via upload
    First App version (Backend + database built + basic frontend buit)

---
+++ myinsurancehub/contacts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import contact_list
+
+
+
+urlpatterns = [
+    path('', contact_list, name='contact_list'),
+]
---
+++ myinsurancehub/contacts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Contact
+
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
